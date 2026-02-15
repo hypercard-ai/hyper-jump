3a94d462a529738a64d70410a8681eb7d7bae013
Ian Henry <mail@ianhenry.dev>
2026-02-14 19:21:41 -0500

    Fix loading and error pages

--- src/error-page.tsx
+++ src/error-page.tsx
@@ -1,12 +1,3 @@
-import { PAGE_HEIGHT, PAGE_WIDTH } from "./constants";
-
 export default function PDFErrorPage() {
-	return (
-		<div
-			className="hj-error"
-			style={{ width: PAGE_WIDTH, height: PAGE_HEIGHT }}
-		>
-			Error loading file
-		</div>
-	);
+	return <div className="hj-error">Error loading file</div>;
 }
--- src/loading-page.tsx
+++ src/loading-page.tsx
@@ -1,11 +1,6 @@
-import { PAGE_HEIGHT, PAGE_WIDTH } from "./constants";
-
 export default function PDFLoadingPage() {
 	return (
-		<div
-			className="hj-loading"
-			style={{ width: PAGE_WIDTH, height: PAGE_HEIGHT }}
-		>
+		<div className="hj-loading">
 			<div className="hj-spinner" />
 		</div>
 	);
