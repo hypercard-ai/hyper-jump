55e7b1143cf045d0d0f902dafd891948834c2fb8
Ian Henry <mail@ianhenry.dev>
2026-02-26 12:03:30 -0500

    Fix type exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,16 +3,10 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/index.ts", "src/pdf/index.ts", "src/video/index.ts"],
 	format: ["esm"],
-	dts: true,
+	dts: false,
 	sourcemap: true,
 	clean: true,
-	external: [
-		"react",
-		"react-dom",
-		"react-pdf",
-		"react-window",
-		"@vidstack/react",
-	],
+	external: ["react", "react-dom", "react-pdf", "react-window"],
 	splitting: false,
 	minify: false,
 });
